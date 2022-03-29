712062ab087c221be51506548dd4df2e766fec78
remote007 <59466080+remote007@users.noreply.github.com>
2022-03-29 11:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from urllib import response
 from flask import Flask
 import json
+import logging
+
 app = Flask(__name__)
 
 @app.route("/status")
-def status():
+def healthcheck():
     response = app.response_class(
         response = json.dumps({"result":"OK-Healthy"}),
         status = 200,
         mimetype = 'application/json'
     )
+    app.logger.info("status checked successfully")
     return response
 
 @app.route("/metrics")
@@ -19,11 +22,14 @@ def metrics():
         status = 200,
         mimetype = 'application/json'
     )
+    app.logger.info("metric request succesful")
     return response
 
 @app.route("/")
 def hello():
     return "Hello World!"
+    app.logger.info("main request successful")
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
